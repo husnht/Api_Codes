b23af996d4eecda365c312fea580e508e8892fe0
Hus.Nht <hus.nht@gmail.com>
2022-11-03 23:27:05 +0300

    03.11.2022

--- src/test/java/base_urls/JsonplaceholderBaseUrl.java
+++ src/test/java/base_urls/JsonplaceholderBaseUrl.java
@@ -1,15 +1,15 @@
-package base_url;
+package base_urls;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
-public class GoRestBaseUrl {
+public class JsonplaceholderBaseUrl {
     protected RequestSpecification spec;
 
     @Before
     public void setup() {
-        spec = new RequestSpecBuilder().setBaseUri("https://gorest.co.in/public/v1/").build();
+        spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com").build();
     }
 
 }
