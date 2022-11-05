e011694124c274787b89bee250501c8517483d43
Hus.Nht <hus.nht@gmail.com>
2022-11-05 23:19:40 +0300

    05.11.2022

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -24,7 +24,16 @@ public class JsonPlaceHolderTestData {
         return expectedDataMap;
     }
 
+    public String expectedDataInString (int userId, String title, Boolean completed) {//Dinamik expected Data methodu
 
+
+        String expectedData = "{\n" +
+                "                 \"userId\": "+userId+",\n" +
+                "                 \"title\": \""+title+"\",\n" +
+                "                 \"completed\": "+completed+"\n" +
+                "               }";
+        return expectedData;
+    }
 
 
 
